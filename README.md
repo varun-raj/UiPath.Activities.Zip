ed8cd62c8854c093096c9091c6301eadf34e50f8
Varun <varun@skcript.com>
2020-01-24 08:09:39 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### UiPath.Activities.Zip
+With this custom activity for zip & unzip activities. This plugin comes with the following features. 
+
+- Zip
---
+++ UiPath.Activities.Zip/Zip.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO.Compression;
+
+namespace UiPath.Activities.Zip
+{
+    public class UnZip : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> ZipPath { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> OutputFolderPath { get; set; }
+
+        [Category("Output")]
+        [RequiredArgument]
+        public OutArgument<bool> Result { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string zipPath = ZipPath.Get(context);
+            string outputFolderPath = OutputFolderPath.Get(context);
+            bool result;
+
+            try
+            {
+                ZipFile.ExtractToDirectory(zipPath, outputFolderPath);
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+
+            Result.Set(context, result);
+        }
+    }
+
+}
